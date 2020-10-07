76b68f8fe13811f6644cbd2807b4846f9e7a090d
metabare <111348163+metabare@users.noreply.github.com>
2020-10-07 15:18:20 +0000

    Update sphinx from 1.5.1 to 1.5.2

---
+++ Source/ThirdParty/RangeV3/RangeV3.Build.cs
@@ -0,0 +1,16 @@
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class RangeV3 : ModuleRules
+{
+	public RangeV3(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Range-V3-VS2015/include"));
+	}
+}
