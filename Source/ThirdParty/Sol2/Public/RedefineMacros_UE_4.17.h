555f9310dd573ddd6388563359895b5093e6aa7c
metabare <111348163+metabare@users.noreply.github.com>
2021-02-28 18:25:33 +0000

    trailing whitespace

--- Source/ThirdParty/Sol2/Public/RedefineMacros_UE_4.17.h
+++ Source/ThirdParty/Sol2/Public/RedefineMacros_UE_4.17.h
@@ -1731,4 +1731,15 @@
 #pragma pop_macro("GL_COMPUTE_LOCAL_WORK_SIZE")
 #pragma pop_macro("GL_COMPRESSED_SRGB8_ETC2")
 #pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC")
-#pragma pop_macro("GL_COMPRESSED_SRGB8_A
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x8_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x6_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x5_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x6_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x5_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x5_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x4_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x10_KHR")
+#pragma pop_macro("GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x8_KHR")
+#pragma po
