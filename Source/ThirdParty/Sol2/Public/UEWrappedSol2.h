013fbe58bff5d38df832aa7921b53655057f9228
metabare <111348163+metabare@users.noreply.github.com>
2021-05-14 16:56:09 +0000

    USB: option: add device ID for SpeedUp SU9800 usb 3g modem

---
+++ Source/ThirdParty/Sol2/Public/UEWrappedSol2.h
@@ -0,0 +1,17 @@
+
+//#define SOL_EXCEPTIONS_SAFE_PROPAGATION   // Do we need this? At least seems to work without..
+//#define SOL_NO_EXCEPTIONS   // LuaJIT and exceptions did not play together: https://sol2.readthedocs.io/en/latest/exceptions.html
+                            // ..but do now; check the link above!
+//#ifndef NDEBUG   // what macro to use with UE?
+//#define SOL_CHECK_ARGUMENTS
+//#endif
+
+
+#pragma warning( push )
+#pragma warning( disable: 4628 )
+
+#include "UndefineMacros_UE_4.17.h"
+#include "sol2/sol.hpp"
+#include "RedefineMacros_UE_4.17.h"
+
+#pragma warning( pop )
---
+++ Source/ThirdParty/Sol2/Public/UndefineMacros_UE_4.17.h
@@ -0,0 +1,7 @@
+#pragma push_macro("ABSTRACT")
+#undef ABSTRACT
+#pragma push_macro("AC_UnalignedSwap")
+#undef AC_UnalignedSwap
+#pragma push_macro("ACCUM_LOADTIME")
+#undef ACCUM_LOADTIME
+#pragma push_macro("ADD_LATENT_AUTOMATION
