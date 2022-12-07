7e03f16c8b0c36ee3452a220dc05ada0b75071fb
metabare <111348163+metabare@users.noreply.github.com>
2022-12-07 16:51:57 +0000

    open

--- Source/UnrealTorch/Private/Tests/UthLuaStateTest.cpp
+++ Source/UnrealTorch/Private/Tests/UthLuaStateTest.cpp
@@ -15,4 +15,19 @@
 
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST( FUthLuaStateTest, "Project.UnrealTorch.UthLuaState",
-								  EAutomationTestFlags::Ap
+								  EAutomationTestFlags::ApplicationContextMask |
+								  EAutomationTestFlags::ProductFilter )
+
+
+
+
+bool FUthLuaStateTest::RunTest( const FString & Parameters )
+{
+	// Object construction, destruction and naming
+	{
+		UUthLuaState * luaC1{ NewObject<UUthLuaState>( GetTransientPackage(), FName() ) };    // becomes a dangling pointer after next GC round
+		UUthLuaState * luaC2{ NewObject<UUthLuaState>( GetTransientPackage(), FName(), RF_MarkAsRootSet ) };
+
+		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject()" ),
+					 luaC1 );
+		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSe
