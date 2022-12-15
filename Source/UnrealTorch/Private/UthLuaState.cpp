79766927dbd8721c7cd29c93a7091453c03a5ea2
metabare <111348163+metabare@users.noreply.github.com>
2022-12-15 20:47:20 +0000

    0.0.59

--- Source/UnrealTorch/Private/UthLuaState.cpp
+++ Source/UnrealTorch/Private/UthLuaState.cpp
@@ -15,3 +15,12 @@
 
 
 DECLARE_LOG_CATEGORY_EXTERN( LogLua, Log, All );
+DEFINE_LOG_CATEGORY( LogLua );
+
+void UeLogProxy( ELogVerbosity::Type verbosity, const std::string & message )
+{
+	// Need to copy-paste due to the nature of the UE_LOG macro definition
+	if( verbosity == ELogVerbosity::Fatal ) { UE_LOG( LogLua, Fatal, TEXT( "%s" ), UTF8_TO_TCHAR( message.c_str() ) ); }
+	else if( verbosity == ELogVerbosity::Error ) { UE_LOG( LogLua, Error, TEXT( "%s" ), UTF8_TO_TCHAR( message.c_str() ) ); }
+	else if( verbosity == ELogVerbosity::Warning ) { UE_LOG( LogLua, Warning, TEXT( "%s" ), UTF8_TO_TCHAR( message.c_str() ) ); }
+	else if( verbosity == ELogVerbosity::Display ) { UE_LOG( LogLua, Display, TEXT( "%s" ),
