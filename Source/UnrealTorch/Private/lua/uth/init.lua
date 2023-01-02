a470c55bb31b87800128c66ea984332b78f73b23
metabare <111348163+metabare@users.noreply.github.com>
2023-01-02 14:22:04 +0000

    Improve the documentation.

--- Source/UnrealTorch/Private/lua/uth/init.lua
+++ Source/UnrealTorch/Private/lua/uth/init.lua
@@ -11,4 +11,16 @@
 --       .UE_LOG( verbosity, message )    Write log entries to UE log
 --       .ELogVerbosity                   Verbosity level enumeration for UE_LOG()
 --       .BuildShippingOrTest             True if UE is built in Shipping or Test configuration
---       .FPaths                          UE p
+--       .FPaths                          UE path information
+--         .GameLogDir                    Log directory path from FPaths::GameLogDir()
+--
+-- Output from io.write() and print() is directed to a log file in the UE log directory 'Saved\Logs\'.
+--
+-- The utility module is imported into 'uth.utility'. A generic logger from the utility module is copied, with its
+-- verbosity level enumeration, to the global variable LOG and ELogVerbosity. Usage:
+--   LOG( ELogVerbosity.Warning, 'Log message here' )
+--
+-- Torch is imported into the global variable 'torch'.
+
+uth.ue.UE_LOG( uth.ue.ELogVerbosity.Log, '[init.lua] Initializing Lua state..' )
+assert( uth, 'The global variable \'uth\'
