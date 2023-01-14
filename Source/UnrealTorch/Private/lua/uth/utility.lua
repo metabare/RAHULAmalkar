76814a6dcdadc631553545c592315cbe94abeda2
metabare <111348163+metabare@users.noreply.github.com>
2023-01-14 20:06:49 +0000

    _automated cleaning tests version comment spaces

--- Source/UnrealTorch/Private/lua/uth/utility.lua
+++ Source/UnrealTorch/Private/lua/uth/utility.lua
@@ -25,4 +25,27 @@ end
 --- Log to io.output()
 utility.LUA_LOG = (
   function()
-    local ELogVerbosity_inv = tomap( map( funct
+    local ELogVerbosity_inv = tomap( map( function(k,v) return v, k end, getmetatable(uth.ue.ELogVerbosity).__index ) )
+
+    return function( verbosity, message )
+      io.write( '[' .. ELogVerbosity_inv[verbosity] .. '] ' .. message .. '\n' )
+      io.flush()
+    end
+  end
+)()
+
+
+
+
+--- The default logger
+--
+-- Log to LUA_LOG and optionally also to UE_LOG. The latter is controlled by the uth.LOG_ALSO_TO_UE flag.
+utility.LOG = (
+  function()
+    -- We don't want later output decorations to affect our loggers
+    local LUA_LOG_capture = utility.LUA_LOG
+    local UE_LOG_capture  = uth.ue.UE_LOG
+
+    return function(...)
+      LUA_LOG_capture(...)
+      if uth.LOG_ALSO_TO_UE the
