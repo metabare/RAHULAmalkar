844714a1638c6ef34ea0d91b55ee67288e7b7fb2
metabare <111348163+metabare@users.noreply.github.com>
2023-01-14 13:01:27 +0000

    IGNORE

--- Source/UnrealTorch/Private/lua/uth/tests.lua
+++ Source/UnrealTorch/Private/lua/uth/tests.lua
@@ -22,4 +22,22 @@ function tests.run()
   uth.ue.UE_LOG( ELogVerbosity.Log, 'Testing UE_LOG().' )
   uth.utility.LUA_LOG( ELogVerbosity.Log, 'Testing LUA_LOG().' )
   LOG( ELogVerbosity.Log, 'Testing generic LOG().' )
-  function ne
+  function nestedfunc1()
+    local nestedfunc1_local = 1
+    function nestedfunc2()
+      local nestedfunc2_local = nestedfunc1_local + 1
+      function nestedfunc3()
+        local nestedfunc3_local = nestedfunc2_local + 1
+        local upvals = uth.utility.upvalues()
+        LOG( ELogVerbosity.Log, 'Testing callstack logging.' )
+        LOG( ELogVerbosity.Log, 'Testing upvalues(): ' .. tests.inspect( upvals ) )
+      end
+      nestedfunc3()
+    end
+    nestedfunc2()
+  end
+  nestedfunc1()
+
+  -- Test locals and alllocals querying
+  io.write( 'Inspecting locals and alllocals\n' )
+  io.write( 'locals: \n' .. tests.inspect( uth.utility.locals() ) ..
