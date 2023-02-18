b2276a4a06e51315f8d11fa0204737b0f1a9d3e1
metabare <111348163+metabare@users.noreply.github.com>
2023-02-18 19:06:39 +0000

    more filters

--- Source/UnrealTorch/Public/UthBlueprintStatics.h
+++ Source/UnrealTorch/Public/UthBlueprintStatics.h
@@ -23,4 +23,12 @@ class UNREALTORCH_API UUthBlueprintStatics : public UBlueprintFunctionLibrary
 public:
 
 	// comment block copy at UUthLuaState::UUthLuaState()
-	/** Constructs a new UthLuaState object, creating a new Lua state and initial
+	/** Constructs a new UthLuaState object, creating a new Lua state and initializing it.
+	 *
+	 * Blueprint users: Make sure to immediately store the returned object in a variable. Using the return value
+	 * directly by pulling wires from the return node works only as long as no-one and nothing triggers the garbage
+	 * collector; Blueprint return nodes _do not_ hold and protect objects from being collected.
+	 *
+	 * C++ users: Use factory helpers like UUthBlueprintStatics::CreateLuaState() or UE's NewObject() to create
+	 * instances of this class. Never explicitly delete these instances; instead use the destroy() method. See the
+	 * section below on object lifecycle for mo
